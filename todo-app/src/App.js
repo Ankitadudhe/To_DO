290b5c332f48ab8186aaaf8dbaf7e7d79d2bd131
Ankitadudhe <ankitadudhe95@gmail.com>
2020-08-12 18:58:13 +0530

    modify in App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,18 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
-import Todo from './Todo/Todo';
-// import SimpleSlider from './SimpleSlick'
-// import SlideView from './SlideView'
-// import Button from './component/button/button'
-// const AppWrapper = styled.div
-// `display:flex ;
-// justify-content:center;`;
-
+import Todo from './ToDoList/Todo'
 function App() {
     return (
-         < div className = "App" >
-        <Todo /> { /* <SimpleSlider/> */ } { /* <AppWrapper> */ } { /* <SlideView/> */ } { /* </AppWrapper> */ } { /* <Button label="click me plz"></Button> */ } 
+         < div className = "main_div" >
+         <Todo/>
         </div>
     );
 }
